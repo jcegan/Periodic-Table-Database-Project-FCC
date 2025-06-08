4d11a26d0a5a2ae5cd356e39ada563a80c78f5bf
Joel Egan <jcegan22@gmail.com>
2025-06-08 20:10:01 +0000

    fix: read function

--- element.sh
+++ element.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
 else 
-  PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+  PSQL="psql --username=freecodecamp --dbname=periodic_table --tuples-only --no-align -c"
   ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = '$1' OR symbol = '$1' OR name = '$1'")
-  if [[ ! -z $ATOMIC_NUMBER ]]
+  if [[ $ATOMIC_NUMBER ]]
   then
-    echo $($PSQL "SELECT * FROM elements FULL JOIN properties ON elements.atomic_number = properties.atomic_number WHERE elements.atomic_number = $ATOMIC_NUMBER") | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME ATOMIC_NUMBER TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE_ID
+    RETRIEVE_INFO=$($PSQL "SELECT * FROM elements FULL JOIN properties ON elements.atomic_number = properties.atomic_number WHERE elements.atomic_number = $ATOMIC_NUMBER")
+    echo "$RETRIEVE_INFO" | while IFS='|' read ATOMIC_NUMBER SYMBOL NAME ATOMIC_NUMBER TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE_ID
     do
-      echo "$TYPE $MELTING_POINT"
+      echo "test $TYPE"
     done
   fi
 fi
