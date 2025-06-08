7cde9dce495e9120df6e1a39c94e862479b95a87
Joel Egan <jcegan22@gmail.com>
2025-06-08 19:43:41 +0000

    fix: Fix psql query

--- element.sh
+++ element.sh
@@ -3,11 +3,13 @@ if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
 else 
-  PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+  PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
   ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = '$1' OR symbol = '$1' OR name = '$1'")
   if [[ ! -z $ATOMIC_NUMBER ]]
   then
-    echo $($PSQL "SELECT * FROM elements FULL JOIN properties ON elements(atomic_number) = properties(atomic_number) WHERE atomic_number = $ATOMIC_NUMBER") | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME ATOMIC_NUMBER TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE_ID
-    echo "$TYPE $MELTING_POINT"
+    echo $($PSQL "SELECT * FROM elements FULL JOIN properties ON elements.atomic_number = properties.atomic_number WHERE elements.atomic_number = $ATOMIC_NUMBER") | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME ATOMIC_NUMBER TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE_ID
+    do
+      echo "$TYPE $MELTING_POINT"
+    done
   fi
 fi
