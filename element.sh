3a32ca1f0beb325aa2c1fd80afea06f80a54b830
Joel Egan <jcegan22@gmail.com>
2025-06-08 20:29:41 +0000

    feat: added invalid parameter message

--- element.sh
+++ element.sh
@@ -12,6 +12,7 @@ else
   else
     ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$1' OR name = '$1'")
   fi
+
   if [[ $ATOMIC_NUMBER ]]
   then
     RETRIEVE_INFO=$($PSQL "SELECT * FROM elements FULL JOIN properties ON elements.atomic_number = properties.atomic_number WHERE elements.atomic_number = $ATOMIC_NUMBER")
@@ -19,5 +20,7 @@ else
     do
       echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     done
+  else
+    echo "I could not find that element in the database."
   fi
 fi
